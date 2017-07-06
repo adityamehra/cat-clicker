d309ecfb3fa830d95cfbe5dae834cbc7a9cc6e51
Aditya Mehra <mehraaditya71@gmail.com>
2017-07-05 17:44:36 -0700

    Added knockoutjs with data-bind

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var ViewModel = function() {
+ this.name = ko.observable('Tabby');
+ this.clickCount = ko.observable(0);
+ this.imgSrc = ko.observable('./img/22252709_010df3379e_z.jpg');
+ this.imgAttribution = ko.observable('Flickr');
+
+ this.incrementCounter = function() {
+  this.clickCount(this.clickCount() + 1);
+ };
+
+ // this.level = ko.computed(function() {
+ //  if(this.clickCount() == 0){
+ //   return 'Newborn';
+ //  }else if(this.clickCount() > 0 && this.clickCount() <= 10) {
+ //   return 'Infant';
+ //  }else if(this.clickCount() > 10 && this.clickCount() <= 19) {
+ //   return 'Teen';
+ //  }else{
+ //   return 'Adult';
+ //  }
+ // }, this);
+
+}
+
+ko.applyBindings(new ViewModel());
